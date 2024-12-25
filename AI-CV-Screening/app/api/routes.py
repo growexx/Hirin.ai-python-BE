0a4035958694f0f235b1f111c777d3bd0b189be4
apple <apple@apples-MacBook-Pro.local>
2024-12-25 13:09:58 +0530

    added threading logic for api response

--- AI-CV-Screening/app/api/routes.py
+++ AI-CV-Screening/app/api/routes.py
@@ -1,20 +1,20 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,after_this_request
 from app import auth
 from app.utils.config_loader import Config
 from pydantic import BaseModel, Field, ValidationError
 from app.dto.dto import UserLoginDTO, ProtectedRouteResponseDTO, PublicRouteResponseDTO
+from app.services.putQueueMessageService import PutMessageQueue
+import threading
 
 api_blueprint = Blueprint('api', __name__)
 
 username = Config.get('Auth','username')
 password = Config.get('Auth','password')
 
-# Dummy data for authentication
 users = {
     username : password  
 }
 
-# Define the authentication function
 @auth.verify_password
 def verify_password(username, password):
     if users.get(username) == password:
@@ -24,13 +24,22 @@ def verify_password(username, password):
 @auth.login_required
 def protected():
     response_dto = ProtectedRouteResponseDTO(message="This is a protected route")
+    print("Inside protected method")
+    thread = threading.Thread(target=PutMessageQueue.putMessage)
+    thread.daemon = True 
+    thread.start()
+    print("Complete protected method")
     return jsonify(response_dto.dict())
 
-# Public route
 @api_blueprint.route('/public')
 def public():
     response_dto = PublicRouteResponseDTO(message="This is a public route")
-    return jsonify(response_dto.dict())
+    print("Inside public method")
+    thread = threading.Thread(target=PutMessageQueue.putMessage)
+    thread.daemon = True 
+    thread.start()
+    print("Complete public method")
+    return jsonify(response_dto.dict()), 200
 
 
 
